318cc46993ae6256e4d2098523654fb5d3c3541a
Angela Mitchell <anglarenmit@gmail.com>
2022-02-12 15:25:16 -0600

    finish standard requirements for studio

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // TODO: add code here
+window.addEventListener('load', () => {
+  const container = document.getElementById('container');
+  fetch(
+    'https://handlers.education.launchcode.org/static/astronauts.json'
+  ).then((response) => {
+    response.json().then((json) => {
+      console.log(json);
+      json.forEach((astronaut) => {
+        container.innerHTML += `
+            <div class="astronaut">
+                <div class="bio">
+                    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                        <li>Active: ${astronaut.active}</li>
+                        <li>Skills: ${astronaut.skills.join(', ')}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="${astronaut.picture}">
+            </div>
+        `;
+      });
+    });
+  });
+});

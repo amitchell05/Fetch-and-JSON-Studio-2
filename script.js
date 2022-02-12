0019f1ec7497e34e1547bbaa9e4dc88efbf8fcfd
Angela Mitchell <anglarenmit@gmail.com>
2022-02-12 15:48:23 -0600

    finish bonus missions

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-// TODO: add code here
 window.addEventListener('load', () => {
   const container = document.getElementById('container');
+  const title = document.getElementById('title');
+
   fetch(
     'https://handlers.education.launchcode.org/static/astronauts.json'
   ).then((response) => {
     response.json().then((json) => {
-      console.log(json);
-      json.forEach((astronaut) => {
+      title.innerHTML += `: ${json.length}`;
+
+      json = json.sort((a, b) => (a.hoursInSpace < b.hoursInSpace ? 1 : -1));
+
+      json.forEach((astronaut, index) => {
         container.innerHTML += `
             <div class="astronaut">
                 <div class="bio">
                     <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
                     <ul>
                         <li>Hours in space: ${astronaut.hoursInSpace}</li>
-                        <li>Active: ${astronaut.active}</li>
+                        <li id="activeStatus${index + 1}">Active: ${
+          astronaut.active
+        }</li>
                         <li>Skills: ${astronaut.skills.join(', ')}</li>
                     </ul>
                 </div>
                 <img class="avatar" src="${astronaut.picture}">
             </div>
         `;
+        const activeStatus = document.getElementById(
+          `activeStatus${index + 1}`
+        );
+        if (astronaut.active) {
+          activeStatus.style.color = 'green';
+        }
       });
     });
   });
